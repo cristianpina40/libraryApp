265d09cea5de832fce9587f07a7aa3be3e7c6977
Cristian Pina <cp1003009@gmail.com>
2023-04-09 15:34:39 -0700

    Basic functionality added for app

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 var addNewBook = document.getElementById('fContainer');
 
+let place = 0;
+
 let myLibrary = [];
 
 function Book(title, author, pages) {
@@ -21,15 +23,61 @@ function addNew(){
 
 function addBookToLibrary(){
 
-    let length = myLibrary.length;
+    
 
-    for(let i = 0; i < length; i++)
+    
+    let length = myLibrary.length;
+    
+    
+
+    for(let i = place; i < length; i++)
     {
+
+        let parentDiv = document.getElementById('main'); // get the parent element
+        let cardDiv = document.createElement('div');
+        cardDiv.className = 'card';
+        parentDiv.appendChild(cardDiv); 
+
+        let nameDiv = document.createElement('div');
+        nameDiv.className = 'bname';
+        cardDiv.appendChild(nameDiv);
+        let namePara = document.createElement('p');
+        namePara.className = "bookName";
+        namePara.innerHTML = myLibrary[i].title;
+        nameDiv.appendChild(namePara);
+
+        let authorDiv = document.createElement('div');
+        authorDiv.className = "bauthor";
+        cardDiv.appendChild(authorDiv);
+        let authorPara = document.createElement('p');
+        authorPara.className = "bookAuthor";
+        authorPara.innerHTML = myLibrary[i].author;
+        authorDiv.appendChild(authorPara);
+
+        let pagesDiv = document.createElement('div');
+        pagesDiv.className = 'bpages';
+        cardDiv.appendChild(pagesDiv);
+        let pagesPara = document.createElement('p');
+        pagesPara.className = "bookPages";
+        pagesPara.innerHTML = myLibrary[i].pages;
+        pagesDiv.appendChild(pagesPara);
+
+        let readButton = document.createElement('button');
+        readButton.textContent = 'Not Read'; 
+        readButton.id = 'readOrNot';
+        cardDiv.appendChild(readButton);
+
+        let removeButton = document.createElement('button');
+        removeButton.textContent = 'Remove'; 
+        removeButton.id = 'remove';
+        cardDiv.appendChild(removeButton);
+
         console.log(myLibrary[i].title);
         console.log(myLibrary[i].author);
         console.log(myLibrary[i].pages);
     }
 
+    place++;
 
 }
 
