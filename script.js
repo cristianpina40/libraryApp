8f8e120a865f106712c4643b88f2ac2e026563c4
Cristian Pina <cp1003009@gmail.com>
2023-04-08 23:14:51 -0700

    Added styling, form, button

---
+++ script.js
@@ -0,0 +1,9 @@
+function openForm() {
+    document.getElementById("popupForm").style.display = "block";
+    document.getElementById("overlay").style.display = "block";
+  }
+  function closeForm() {
+    document.getElementById("popupForm").style.display = "none";
+    document.getElementById("overlay").style.display = "none";
+  }
+
